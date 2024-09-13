679d894bb933870dea5a528d3c859f03ffa8a80a
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-09-13 16:32:21 +0100

    replace pylint and black with ruff and ruff-format

--- format_multiple_errors/__init__.py
+++ format_multiple_errors/__init__.py
@@ -4,10 +4,14 @@
 
 import warnings
 
-from .formatter import format_multiple_errors
+from .formatter import format_multiple_errors as format_multiple_errors
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    from .pandas import format_column_errors, format_dataframe_errors, ColumnSpec
+    from .pandas import (
+        format_column_errors as format_column_errors,
+        format_dataframe_errors as format_dataframe_errors,
+        ColumnSpec as ColumnSpec,
+    )
 
 del warnings
